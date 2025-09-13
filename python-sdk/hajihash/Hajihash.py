840d28825f6de5bc303a8b2201905f8b5f20c60c
DanielQu9_mac <zxc86796139@gmail.com>
2025-09-13 21:03:59 +0800

    feat: 新增哈呀库文档及相关模块

--- python-sdk/hajihash/Hajihash.py
+++ python-sdk/hajihash/Hajihash.py
@@ -5,43 +5,43 @@
 from hashlib import sha3_256
 from typing import Any
 
-type Message = Any
-type Hash = list[int]
+type Message = str
+type HashList = list[int]
 ENCODE = "utf-8"
 
 
-def sha256(message: Message) -> Hash:
+def sha256(message: Message) -> HashList:
     """
     根据输入获取完整哈基摘要\n
-    @param message 任意消息\n
+    @param message 字串消息\n
     :return 完整长度的哈基摘要\n
     """
-    return list(sha3_256(str(message).encode(ENCODE)).digest())
+    return list(sha3_256(message.encode(ENCODE)).digest())
 
 
-def checkWord(message: Message) -> Hash:
+def checkWord(message: Message) -> HashList:
     """
     根据输入获取哈基校验词\n
-    :param message 任意消息\n
+    :param message 字串消息\n
     :return 两位校验位\n
     """
-    return list(sha3_256(str(message).encode(ENCODE)).digest())[:2]
+    return sha256(message)[:2]
 
 
-def verifySha256(message: Message, hash: Hash) -> bool:
+def verifySha256(message: Message, hash: HashList) -> bool:
     """
     根据完整哈基摘要校验输入\n
-    :param message 任意消息\n
+    :param message 字串消息\n
     :param hash 完整长度的哈基摘要\n
     :return 校验结果(boolean)\n
     """
     return sha256(message) == hash
 
 
-def verifyCheckWord(message: Message, hash: Hash) -> bool:
+def verifyCheckWord(message: Message, hash: HashList) -> bool:
     """
     根据哈基校验词校验输入\n
-    :param message 任意消息\n
+    :param message 字串消息\n
     :param hash 哈基校验词\n
     :return 校验结果(boolean)
     """
@@ -55,8 +55,3 @@ def arrayEqual(a: list[Any], b: list[Any]):
     :param b 串列B
     """
     return a == b
-
-
-if __name__ == "__main__":
-    print(sha256("apple"))
-    print(checkWord("apple"))
